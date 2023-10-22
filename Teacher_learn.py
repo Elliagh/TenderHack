582337baf92597e6fd3d0aefe88875ff0480eec9
Kostan <6PwCnSEPh2NHrEA>
2023-10-22 13:14:24 +0300

    remove lematization

--- Teacher_learn.py
+++ Teacher_learn.py
@@ -1,54 +1,73 @@
 import pandas as pd
 import chardet
 import nltk
-from Helpers import Tokens as tokens
+from sklearn import svm
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import train_test_split
+
 with open('Resources\Logs.csv', 'rb') as f:
     enc = chardet.detect(f.read())
-columns = ["id", "create_date", "log"]
+columns = ["id", "create_date", "log", "type"]
 data = pd.read_csv('Resources\Logs.csv', encoding = enc['encoding'], delimiter=";", usecols=columns)
+print(data)
 nltk.download("stopwords") # поддерживает удаление стоп-слов
 nltk.download('punkt') # делит текст на список предложений
 nltk.download('wordnet') # проводит лемматизацию
 from nltk.corpus import stopwords
 data['log'][31]
 new_log = []
-lemmatize = nltk.WordNetLemmatizer()
-for text_line in data['log']:
-    text = nltk.word_tokenize(text_line)
-    text = [lemmatize.lemmatize(word) for word in text_line]
-    text = "".join(text)
-    new_log.append(text)
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfVectorizer
+
+
 count = CountVectorizer()
-values = count.fit_transform(new_log).toarray()
-import numpy as np
-types = []
-not_found_str = []
-for text_line in data['log']:
-    exist_token = False
-    text_line = text_line.lower()
-    dict_token = {}
-    for token in tokens.tokens:
-        if token in text_line:
-            exist_token = True
-            types.append(tokens.tokens[token])
-            break
-    if exist_token == False:
-        print(text_line)
-from sklearn.naive_bayes import GaussianNB
-from sklearn.model_selection import train_test_split
+values = count.fit_transform(data['log']).toarray()
+
 X = values
-Y = types
-x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.2, random_state = 38)
-nb = GaussianNB()
-result_bayes = nb.fit(x_train, y_train)
-print(nb.score(x_test,y_test))
-from sklearn.linear_model import LogisticRegression
-logreg = LogisticRegression()
-result_logreg = logreg.fit(x_train, y_train)
-print(logreg.score(x_test,y_test))
-from sklearn import svm
-metodsvm = svm.SVC()
-result_svm = metodsvm.fit(x_train, y_train)
-print(metodsvm.score(x_test, y_test))
+Y = data['type']
+
+
+
+
+
+def methods_learn(X,Y, randomstate):
+    x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.1, random_state = randomstate)
+    nb = GaussianNB()
+    result_bayes = nb.fit(x_train, y_train)
+    logreg = LogisticRegression()
+    result_logreg = logreg.fit(x_train, y_train)
+    metodsvm = svm.SVC()
+    result_svm = metodsvm.fit(x_train, y_train)
+    return [nb.score(x_test,y_test), logreg.score(x_test,y_test), metodsvm.score(x_test,y_test)]
+
+result = []
+for i in range(1, 300):
+    results = methods_learn(X,Y,i)
+    result.append(results)
+
+res_baues = []
+res_log_reg = []
+res_svm = []
+
+for res in result:
+    res_baues.append(res[0])
+    res_log_reg.append(res[1])
+    res_svm.append(res[2])
+
+
+plt.subplot(2, 2, 1)
+plt.title("Наивный Байес")
+plt.plot(res_baues)
+
+plt.subplot(2, 2, 2)
+plt.title("Логистическая регрессия")
+plt.plot(res_log_reg)
+
+plt.subplot(2, 2, 3)
+plt.title("Опорный вектора")
+plt.plot(res_log_reg)
+
+plt.show()
