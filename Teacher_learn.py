8a5370af26cce560e0850236326b5a06808e2014
Kostan <6PwCnSEPh2NHrEA>
2023-10-22 16:16:16 +0300

    you god damn right

--- Teacher_learn.py
+++ Teacher_learn.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from nltk.corpus import stopwords
 import chardet
 import nltk
 from sklearn import svm
@@ -12,29 +13,24 @@ from sklearn.model_selection import train_test_split
 
 with open('Resources\Logs.csv', 'rb') as f:
     enc = chardet.detect(f.read())
-columns = ["id", "create_date", "log", "type"]
+columns = ["id", "create_date", "log", "classification"]
 data = pd.read_csv('Resources\Logs.csv', encoding = enc['encoding'], delimiter=";", usecols=columns)
-print(data)
 nltk.download("stopwords") # поддерживает удаление стоп-слов
 nltk.download('punkt') # делит текст на список предложений
 nltk.download('wordnet') # проводит лемматизацию
-from nltk.corpus import stopwords
-data['log'][31]
-new_log = []
-
 
 count = CountVectorizer()
-values = count.fit_transform(data['log']).toarray()
+values = count.fit_transform(data["classification"]).toarray()
 
 X = values
-Y = data['type']
+Y = data["classification"]
 
 
 
 
 
 def methods_learn(X,Y, randomstate):
-    x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.1, random_state = randomstate)
+    x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.25, random_state = randomstate)
     nb = GaussianNB()
     result_bayes = nb.fit(x_train, y_train)
     logreg = LogisticRegression()
@@ -44,8 +40,9 @@ def methods_learn(X,Y, randomstate):
     return [nb.score(x_test,y_test), logreg.score(x_test,y_test), metodsvm.score(x_test,y_test)]
 
 result = []
-for i in range(1, 300):
+for i in range(1, 10):
     results = methods_learn(X,Y,i)
+    print(f"progress:{i}")
     result.append(results)
 
 res_baues = []
@@ -71,3 +68,13 @@ plt.title("Опорный вектора")
 plt.plot(res_log_reg)
 
 plt.show()
+
+
+def divide_on_levels(vector, max_value, min_value, step):
+
+    for point in vector:
+        if point > max_value and point < min_value:
+            continue
+        else:
+            floor = (point - min_value) // step
+
